fcde9c60be02c882ca0839a5b47e1e58fc1e2101
TonyLee <tonylee838@foxmail.com>
2021-02-02 13:30:29 +0800

    Create ReassigmentView component and CollegueList component.

---
+++ meeting-web/src/views/ReassignmentView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <collegue-list></collegue-list>
+  </div>
+</template>
+
+<script>
+import CollegueList from "../components/reassignment/CollegueList";
+
+export default {
+  name: "ReassignmentView",
+  components: { CollegueList },
+  setup() {},
+};
+</script>
+
+<style scoped></style>
