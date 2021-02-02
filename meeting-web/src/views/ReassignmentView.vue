e6866c54cb4c5047efdb6c689909fa9bedfe6dac
TonyLee <tonylee838@foxmail.com>
2021-02-02 14:22:26 +0800

    Create Navbar component.

--- meeting-web/src/views/ReassignmentView.vue
+++ meeting-web/src/views/ReassignmentView.vue
@@ -1,15 +1,17 @@
 <template>
+  <nav-bar title="代会"></nav-bar>
   <div>
     <collegue-list></collegue-list>
   </div>
 </template>
 
 <script>
+import NavBar from "../components/layout/NavBar";
 import CollegueList from "../components/reassignment/CollegueList";
 
 export default {
   name: "ReassignmentView",
-  components: { CollegueList },
+  components: { CollegueList, NavBar },
   setup() {},
 };
 </script>
