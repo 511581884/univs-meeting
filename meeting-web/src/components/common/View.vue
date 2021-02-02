dc8e06d9f8d23616cdb703d48ba26f0c82a230e5
TonyLee <tonylee838@foxmail.com>
2021-02-02 15:10:18 +0800

    Encapsulate logic for displaying navBar and tabBar into a component called View. It takes arguments then determines whether to show navBar or tabBar. Put it into common/ folder

---
+++ meeting-web/src/components/common/View.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav-bar v-if="navBar" :title="navBarTitle"></nav-bar>
+  <slot></slot>
+  <tab-bar v-if="tabBar"></tab-bar>
+</template>
+
+<script>
+import NavBar from "../layout/NavBar.vue";
+import TabBar from "../layout/TabBar.vue";
+
+export default {
+  components: { TabBar, NavBar },
+  name: "View",
+  props: {
+    tabBar: {
+      type: Boolean,
+      default: false,
+    },
+    navBar: {
+      type: Boolean,
+      default: false,
+    },
+    navBarTitle: {
+      type: String,
+    },
+  },
+};
+</script>
