e070adaff08c6629c48889a1ec8d5c6815f59cec
TonyLee <tonylee838@foxmail.com>
2021-02-06 17:00:16 +0800

    Create HistoryMeetingView component.

--- meeting-web/src/views/HistoryMeetingView.vue
+++ meeting-web/src/views/HistoryMeetingView.vue
@@ -1,28 +1,50 @@
 <template>
   <View navBar navBarTitle="历史会议">
-    <div v-for="meeting in meetings" :key="meeting.id">
-      {{ meeting }}
+    <div v-for="key in Object.keys(meetingGroups)" :key="key">
+      <list-header>{{ getDate(key) }}</list-header>
+      <div v-for="(meeting, index) in meetingGroups[key]" :key="meeting.id">
+        <meeting-item
+          :meeting="meeting"
+          :separator="index !== meetingGroups[key].length - 1"
+          @meetingClick="handleMeetingClick"
+        />
+      </div>
     </div>
   </View>
 </template>
 
 <script lang="ts">
-import View from "@/components/common/View.vue";
-import { useStore } from "@/hooks/store";
 import { computed } from "vue";
+import { useRouter } from "vue-router";
+
+import View from "@/components/common/View.vue";
+import ListHeader from "@/components/common/ListHeader.vue";
+import MeetingItem from "@/components/month/MeetingItem.vue";
+import { useStore } from "@/hooks/store";
+import { formatDate } from "@/helpers/dateTime";
 
 export default {
-  components: { View },
+  components: { View, ListHeader, MeetingItem },
   name: "HistoryMeetingView",
   setup() {
     const store = useStore();
-    const meetings = computed(
+    const router = useRouter();
+
+    const meetingGroups = computed(
       () => store.getters["meeting/getCategorizedByDate"]
     );
-    console.log(meetings.value);
+    const getDate = (dateString) => {
+      return formatDate(new Date(dateString), false);
+    };
+
+    const handleMeetingClick = (meetingId) => {
+      router.push(`/details/${meetingId}`);
+    };
 
     return {
-      meetings,
+      getDate,
+      handleMeetingClick,
+      meetingGroups,
     };
   },
 };
