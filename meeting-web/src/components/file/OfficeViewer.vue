e87605b5fea5f34fe9ae1b79df004675592125cf
lyj578 <511581884@qq.com>
2021-02-20 16:15:38 +0800

    modify the way of showing office-file

--- meeting-web/src/components/file/OfficeViewer.vue
+++ meeting-web/src/components/file/OfficeViewer.vue
@@ -1,14 +1,12 @@
 <template>
-  <!--微软在线office文件预览：缺点：加载的时候可能会慢，且较不适合移动端-->
-  <!--
+  <!--微软在线office文件预览：优点是免费，缺点是加载的时候可能会慢，且较不适合移动端-->
   <iframe
     :src="`https://view.officeapps.live.com/op/embed.aspx?src=${url}`"
     id="viewer"
     frameborder="0" 
   ></iframe>
-  -->
-  <!--xdoc在线office文件预览，速度较快-->
-  <iframe :src="`http://view.xdocin.com/xdoc?_xdoc=${url}`" id="viewer"  frameborder="0"/>
+  <!--xdoc在线office文件预览，速度较快，也适合移动端，但是需要付费-->
+  <!--<iframe :src="`http://view.xdocin.com/xdoc?_xdoc=${url}`" id="viewer"  frameborder="0"/>-->
 </template>
 
 <script>
